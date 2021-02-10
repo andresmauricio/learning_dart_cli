83ddc5cb09a56d0ba9be5824d77b4508512a6077
andresmauricio <andresacelas@outlook.com>
2021-02-09 22:22:28 -0500

    functions in dart

--- bin/cli.dart
+++ bin/cli.dart
@@ -4,9 +4,11 @@ import 'package:cli/types.dart' as typesDart;
 import 'package:cli/collections.dart' as collectionDart;
 import 'package:cli/dictionary.dart' as dictionaryDart;
 import 'package:cli/final_and_const.dart' as finalAndCostDart;
+import 'package:cli/fuctions.dart' as funtionsInDart;
 void main(List<String> arguments) {
   // typesDart.learningTypeData();
   // collectionDart.collectionDart();
   // dictionaryDart.dictionaryDart();
-  finalAndCostDart.finalAndConst();
+  // finalAndCostDart.finalAndConst();
+  funtionsInDart.mainFunctions();
 }
---
+++ lib/fuctions.dart
@@ -0,0 +1,19 @@
+void mainFunctions() {
+  add();
+  addWhitparams(5, 3);
+  
+  var result = addWhitRetunr(3, 5);
+  print(result);
+}
+
+void add() {
+  print(5 + 3);
+}
+
+void addWhitparams(a, b) {
+  print(a + b);
+}
+
+int addWhitRetunr(a, b) {
+  return a + b;
+}
